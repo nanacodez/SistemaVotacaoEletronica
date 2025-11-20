bfba10f11fbe52561307835ca2adb0747edcf4f3
anna karolina <156126433+nanacodez@users.noreply.github.com>
2025-11-20 19:30:07 +0000

    comentario

--- main.c
+++ main.c
@@ -1,20 +1,13 @@
-/*
- * ===================================================================
- * ARQUIVO: main.c
- * DESCRIÇÃO: Ponto de entrada (contém apenas o loop principal).
- * ===================================================================
- */
-
 #include "menu.h"
 
 int main() {
     int escolha = -1;
 
     do {
-        // Chama a função que está em menu.c
+        // chama a funcao que esta em menu.c
         escolha = exibirMenuPrincipal();
         
-        // Chama a função que está em menu.c
+        // chama a funcao que esta em menu.c
         processarEscolha(escolha);
         
     } while (escolha != 0);
