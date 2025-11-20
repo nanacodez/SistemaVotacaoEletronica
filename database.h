35568a95ce1a82cfa5d598b6c77964f6f050e2f4
anna karolina <156126433+nanacodez@users.noreply.github.com>
2025-11-20 19:28:43 +0000

    comentario

--- database.h
+++ database.h
@@ -3,19 +3,19 @@
 
 #include "modelos.h"
 
-// --- Getters de Contagem ---
+// getters de contagem 
 int db_getTotalEleitores();
 int db_getTotalCandidatos();
 
-// --- Getters de Objetos ---
+// getters de objetos 
 Eleitor* db_getEleitorPorIndice(int indice);
 Candidato* db_getCandidatoPorIndice(int indice);
 
-// --- Funções de Busca (para validação) ---
+// funcoes de busca (para validacao) 
 Eleitor* db_buscarEleitorPorTitulo(const char* titulo);
 Candidato* db_buscarCandidatoPorNumero(const char* numero);
 
-// --- Funções de Adição ---
+// funcoes de adicao
 bool db_adicionarEleitor(Eleitor novoEleitor);
 bool db_adicionarCandidato(Candidato novoCandidato);
 
