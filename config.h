2fa56b4db943df332e9a6fdd3b1dd282aadd613c
anna karolina <156126433+nanacodez@users.noreply.github.com>
2025-11-20 19:26:05 +0000

    comentario

--- config.h
+++ config.h
@@ -1,25 +1,25 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// --- Bibliotecas Padrao ---
+// bibliotecas padrao 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdbool.h>
 
-// --- 1. HEADERS E CONSTANTES ---
+// headers e constantes
 
-// Limites do nosso banco de dados simulado
+// limites do nosso banco de dados simulado
 #define MAX_ELEITORES 1000
 #define MAX_CANDIDATOS 50
 
-// Regras de negócio para validação
+// regras de negocio para validacao
 #define TAM_TITULO 12
-#define TAM_NUM_CANDIDATO 5 // Ex: 5 dígitos para Deputado Federal
+#define TAM_NUM_CANDIDATO 5 // ex 5 digitos para deputado federal
 #define IDADE_MINIMA_VOTAR 16
 
-// Tamanhos dos campos de string (incluindo o caractere nulo '\0')
+// tamanhos dos campos de string (incluindo o caractere nulo '\0')
 #define TAM_NOME 101
 #define TAM_PARTIDO 51
 
